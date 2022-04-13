82addd011498261497902ef0814c8323009ae361
GUCLU Sefa <sefaaguclu@gmail.com>
2022-04-13 13:39:38 +0200

    user control item bold

--- asimov/NoteItem.Designer.cs
+++ asimov/NoteItem.Designer.cs
@@ -37,10 +37,10 @@
             // 
             this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(535, 56);
+            this.label1.Size = new System.Drawing.Size(468, 42);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mathématiques";
             // 
@@ -48,9 +48,9 @@
             // 
             this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(3, 31);
+            this.label3.Location = new System.Drawing.Point(3, 23);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(420, 25);
+            this.label3.Size = new System.Drawing.Size(368, 19);
             this.label3.TabIndex = 2;
             this.label3.Text = "Moy: 100% Min: 100% Max: 100%";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -60,24 +60,25 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(429, 14);
+            this.label5.Location = new System.Drawing.Point(375, 10);
             this.label5.Name = "label5";
             this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label5.Size = new System.Drawing.Size(65, 28);
+            this.label5.Size = new System.Drawing.Size(51, 21);
             this.label5.TabIndex = 4;
             this.label5.Text = "100%";
             // 
             // NoteItem
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "NoteItem";
-            this.Size = new System.Drawing.Size(538, 56);
+            this.Size = new System.Drawing.Size(471, 42);
             this.ResumeLayout(false);
             this.PerformLayout();
 
